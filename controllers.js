e386cd9f93cfd139226d520f829f406346dad3bc
BarbedWire <barbedwire@BarbedWires-MacBook-Pro.local>
2018-08-02 16:12:17 +0300

    Code reorganization

--- controllers.js
+++ controllers.js
@@ -1,19 +1,24 @@
 // controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 
   $scope.city = cityService.city;
   $scope.$watch('city', function() {
     cityService.city = $scope.city;
   });
 
+  $scope.submit = function() {
+    $location.path('/forecast/2');
+  }
+
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', 
+  ['$scope', '$resource', '$routeParams', 'cityService', 'weatherService', 
+  function($scope, $resource, $routeParams, cityService, weatherService) {
 
   $scope.city = cityService.city;
   $scope.days = $routeParams.days || 2;
-  $scope.weatherAPI = $resource(`http://api.openweathermap.org/data/2.5/forecast?APPID=${appID}`, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-  $scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: $scope.days, units: 'metric' });
+  $scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
 
   $scope.roundTemp = (temp) => { 
     return Math.round(temp); 
--- services.js
+++ services.js
@@ -1,4 +1,11 @@
 //services
 weatherApp.service('cityService', function() {
   this.city = 'Minsk,BY';
-})
+});
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+  this.getWeather = function(city, days) {
+    var weatherAPI = $resource(`http://api.openweathermap.org/data/2.5/forecast?APPID=${appID}`, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    return weatherAPI.get( { q: city, cnt: days, units: 'metric' });
+  };
+}]);
